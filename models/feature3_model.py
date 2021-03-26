c6a5f8acc65fef0826ad3721e760837b49bf01ba
dtheo91 <dustintheobald@msn.com>
2021-03-26 11:03:00 +0000

    feature3

---
+++ models/feature3_model.py
@@ -0,0 +1,306 @@
+import torch
+import itertools
+from util.image_pool import ImagePool
+from .base_model import BaseModel
+from . import networks
+from torchvision import models
+from . import mssim
+from . import mask_extractor
+import numpy as np 
+import cv2 as cv
+import torchvision
+import torch.nn as nn
+
+
+def get_simpson_mask(img):
+    lower_range = np.array([100, 100 ,0])  # Set the Lower range value of color in BGR
+    upper_range = np.array([255,217,15])   # Set the Upper range value of color in BGR
+    mask = cv.inRange(img,lower_range,upper_range) # Create a mask with range
+    result1 = cv.bitwise_and(img,img,mask = mask)
+    
+    lower_range = np.array([200, 200 ,200])  # Set the Lower range value of color in BGR
+    upper_range = np.array([255,255,255])   # Set the Upper range value of color in BGR
+    mask = cv.inRange(img,lower_range,upper_range) # Create a mask with range
+    result2 = cv.bitwise_and(img,img,mask = mask)
+
+    return torchvision.transforms.ToTensor()(torchvision.transforms.Grayscale()(torchvision.transforms.ToPILImage()(np.uint8(result1+result2))))
+
+class FeatureExtractor(nn.Module):
+    
+    def __init__(self, vgg19):
+        super().__init__()
+        self.model = torch.nn.ModuleList()
+        for i in range(31):
+            self.model.append(vgg19.features[i])
+        
+    def forward(self, x):
+        feature_list = []
+        gram_list = []
+        for function in self.model: 
+            x = function(x)
+            feature_list.append(x)
+            flatten_feature = torch.flatten(x.squeeze(0), start_dim=1, end_dim=2)
+            gram_matrix = torch.matmul(flatten_feature,flatten_feature.transpose(0,1))
+            gram_list.append(gram_matrix)
+            
+        return (feature_list, gram_list)
+    
+class FeatureExtractor2(nn.Module):
+    
+    def __init__(self, vgg19):
+        super().__init__()
+        self.model = torch.nn.ModuleList()
+        for i in range(31):
+            self.model.append(vgg19.features[i])
+            
+        self.pool = torch.nn.AvgPool2d(2)
+        
+    def forward(self, x):
+        #feature_list = []
+        #gram_list = []
+        for function in self.model: 
+            x = function(x)
+            #feature_list.append(x)
+            
+        pooled = self.pool(x)
+        flatten_feature = torch.flatten(pooled.squeeze(0), start_dim=1, end_dim=2)
+        gram_matrix = torch.matmul(flatten_feature,flatten_feature.transpose(0,1))
+        #gram_list.append(gram_matrix)
+            
+        return (x, gram_matrix) # (feature_list, gram_list)
+
+class Feature3Model(BaseModel):
+    """
+    This class implements the CycleGAN model, for learning image-to-image translation without paired data.
+
+    The model training requires '--dataset_mode unaligned' dataset.
+    By default, it uses a '--netG resnet_9blocks' ResNet generator,
+    a '--netD basic' discriminator (PatchGAN introduced by pix2pix),
+    and a least-square GANs objective ('--gan_mode lsgan').
+
+    CycleGAN paper: https://arxiv.org/pdf/1703.10593.pdf
+    """
+    @staticmethod
+    def modify_commandline_options(parser, is_train=True):
+        """Add new dataset-specific options, and rewrite default values for existing options.
+
+        Parameters:
+            parser          -- original option parser
+            is_train (bool) -- whether training phase or test phase. You can use this flag to add training-specific or test-specific options.
+
+        Returns:
+            the modified parser.
+
+        For CycleGAN, in addition to GAN losses, we introduce lambda_A, lambda_B, and lambda_identity for the following losses.
+        A (source domain), B (target domain).
+        Generators: G_A: A -> B; G_B: B -> A.
+        Discriminators: D_A: G_A(A) vs. B; D_B: G_B(B) vs. A.
+        Forward cycle loss:  lambda_A * ||G_B(G_A(A)) - A|| (Eqn. (2) in the paper)
+        Backward cycle loss: lambda_B * ||G_A(G_B(B)) - B|| (Eqn. (2) in the paper)
+        Identity loss (optional): lambda_identity * (||G_A(B) - B|| * lambda_B + ||G_B(A) - A|| * lambda_A) (Sec 5.2 "Photo generation from paintings" in the paper)
+        Dropout is not used in the original CycleGAN paper.
+        """
+        parser.set_defaults(no_dropout=True)  # default CycleGAN did not use dropout
+        if is_train:
+            parser.add_argument('--lambda_A', type=float, default=10.0, help='weight for cycle loss (A -> B -> A)')
+            parser.add_argument('--lambda_B', type=float, default=10.0, help='weight for cycle loss (B -> A -> B)')
+            parser.add_argument('--lambda_identity', type=float, default=0.5, help='use identity mapping. Setting lambda_identity other than 0 has an effect of scaling the weight of the identity mapping loss. For example, if the weight of the identity loss should be 10 times smaller than the weight of the reconstruction loss, please set lambda_identity = 0.1')
+
+        return parser
+
+    def __init__(self, opt):
+        """Initialize the CycleGAN class.
+
+        Parameters:
+            opt (Option class)-- stores all the experiment flags; needs to be a subclass of BaseOptions
+        """
+        BaseModel.__init__(self, opt)
+        # specify the training losses you want to print out. The training/test scripts will call <BaseModel.get_current_losses>
+        self.loss_names = ['D_A', 'G_A', 'cycle_A', 'idt_A', 'features_A', 'gram_A', 'mssim_A','D_B', 'G_B', 'cycle_B', 'idt_B', 'features_B', 'gram_B','mssim_B']
+        # specify the images you want to save/display. The training/test scripts will call <BaseModel.get_current_visuals>
+        visual_names_A = ['real_A', 'fake_B', 'rec_A']
+        visual_names_B = ['real_B', 'fake_A', 'rec_B']
+        if self.isTrain and self.opt.lambda_identity > 0.0:  # if identity loss is used, we also visualize idt_B=G_A(B) ad idt_A=G_A(B)
+            visual_names_A.append('idt_B')
+            visual_names_B.append('idt_A')
+
+        self.visual_names = visual_names_A + visual_names_B  # combine visualizations for A and B
+        # specify the models you want to save to the disk. The training/test scripts will call <BaseModel.save_networks> and <BaseModel.load_networks>.
+        if self.isTrain:
+            self.model_names = ['G_A', 'G_B', 'D_A', 'D_B']
+        else:  # during test time, only load Gs
+            self.model_names = ['G_A', 'G_B']
+
+        # define networks (both Generators and discriminators)
+        # The naming is different from those used in the paper.
+        # Code (vs. paper): G_A (G), G_B (F), D_A (D_Y), D_B (D_X)
+        self.netG_A = networks.define_G(4, opt.output_nc, opt.ngf, opt.netG, opt.norm,
+                                        not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
+        self.netG_B = networks.define_G(4, opt.input_nc, opt.ngf, opt.netG, opt.norm,
+                                        not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
+        
+        
+        #self.feature_extractor =  models.resnet50(pretrained=True).to(self.device)
+        #self.feature_extractor.fc = torch.nn.Identity()
+        #self.feature_extractor.avgpool = torch.nn.Identity()
+        
+        vgg19 = models.vgg19(pretrained=True)
+        self.feature_extractor = FeatureExtractor2(vgg19).to(self.device)
+        
+        self.mask_extractor = mask_extractor.BiSeNet(19).to(self.device)
+        self.mask_extractor.load_state_dict(torch.load("79999_iter.pth"))
+        self.mask_extractor.eval()
+        
+        for param in self.feature_extractor.parameters():
+            param.requires_grad = False
+        
+        if self.isTrain:  # define discriminators
+            self.netD_A = networks.define_D(opt.output_nc, opt.ndf, opt.netD,
+                                            opt.n_layers_D, opt.norm, opt.init_type, opt.init_gain, self.gpu_ids)
+            self.netD_B = networks.define_D(opt.input_nc, opt.ndf, opt.netD,
+                                            opt.n_layers_D, opt.norm, opt.init_type, opt.init_gain, self.gpu_ids)
+
+        if self.isTrain:
+            if opt.lambda_identity > 0.0:  # only works when input and output images have the same number of channels
+                assert(opt.input_nc == opt.output_nc)
+            self.fake_A_pool = ImagePool(opt.pool_size)  # create image buffer to store previously generated images
+            self.fake_B_pool = ImagePool(opt.pool_size)  # create image buffer to store previously generated images
+            # define loss functions
+            self.criterionGAN = networks.GANLoss(opt.gan_mode).to(self.device)  # define GAN loss.
+            self.criterionCycle = torch.nn.L1Loss()
+            self.criterionIdt = torch.nn.L1Loss()
+            self.msssim = mssim.MSSSIM(window_size = 8, normalize = True)
+            # initialize optimizers; schedulers will be automatically created by function <BaseModel.setup>.
+            self.optimizer_G = torch.optim.Adam(itertools.chain(self.netG_A.parameters(), self.netG_B.parameters()), lr=opt.lr, betas=(opt.beta1, 0.999))
+            self.optimizer_D = torch.optim.Adam(itertools.chain(self.netD_A.parameters(), self.netD_B.parameters()), lr=opt.lr, betas=(opt.beta1, 0.999))
+            self.optimizers.append(self.optimizer_G)
+            self.optimizers.append(self.optimizer_D)
+
+    def set_input(self, input):
+        """Unpack input data from the dataloader and perform necessary pre-processing steps.
+
+        Parameters:
+            input (dict): include the data itself and its metadata information.
+
+        The option 'direction' can be used to swap domain A and domain B.
+        """
+        AtoB = self.opt.direction == 'AtoB'
+        self.real_A = input['A' if AtoB else 'B'].to(self.device)
+        self.real_B = input['B' if AtoB else 'A'].to(self.device)
+        self.image_paths = input['A_paths' if AtoB else 'B_paths']
+
+    def forward(self):
+        """Run forward pass; called by both functions <optimize_parameters> and <test>."""
+        
+        # FAKE B
+        self.real_mask_A = torch.cat([self.mask_extractor(self.real_A)[0].detach().argmax(1).unsqueeze(0), self.real_A], dim=1)
+        self.fake_B = self.netG_A(self.real_mask_A)  # G_A(A)
+        
+        # RECONSTRUCTION A
+        fake_mask_B = ((self.fake_B + 1) * 255 / 2).long()
+        fake_mask_B = torch.cat([get_simpson_mask(fake_mask_B.squeeze(0).transpose(0,2).detach().cpu().numpy()).unsqueeze(0).to(self.device), self.fake_B], dim=1)
+        self.rec_A = self.netG_B(fake_mask_B)   # G_B(G_A(A))
+        
+        # FAKE A
+        real_mask_B = ((self.real_B + 1) * 255 / 2).long()
+        self.real_mask_B = torch.cat([get_simpson_mask(real_mask_B.squeeze(0).transpose(0,2).detach().cpu().numpy()).unsqueeze(0).to(self.device), self.real_B], dim=1)
+        self.fake_A = self.netG_B(self.real_mask_B)  # G_B(B)
+        
+        # RECONSTRUCTION B
+        fake_mask_A  = torch.cat([self.mask_extractor(self.fake_A)[0].detach().argmax(1).unsqueeze(0), self.fake_A], dim=1)
+        self.rec_B = self.netG_A(fake_mask_A)   # G_A(G_B(B))
+
+    def backward_D_basic(self, netD, real, fake):
+        """Calculate GAN loss for the discriminator
+
+        Parameters:
+            netD (network)      -- the discriminator D
+            real (tensor array) -- real images
+            fake (tensor array) -- images generated by a generator
+
+        Return the discriminator loss.
+        We also call loss_D.backward() to calculate the gradients.
+        """
+        # Real
+        pred_real = netD(real)
+        loss_D_real = self.criterionGAN(pred_real, True)
+        # Fake
+        pred_fake = netD(fake.detach())
+        loss_D_fake = self.criterionGAN(pred_fake, False)
+        # Combined loss and calculate gradients
+        loss_D = (loss_D_real + loss_D_fake) * 0.5
+        loss_D.backward()
+        return loss_D
+
+    def backward_D_A(self):
+        """Calculate GAN loss for discriminator D_A"""
+        fake_B = self.fake_B_pool.query(self.fake_B)
+        self.loss_D_A = self.backward_D_basic(self.netD_A, self.real_B, fake_B)
+
+    def backward_D_B(self):
+        """Calculate GAN loss for discriminator D_B"""
+        fake_A = self.fake_A_pool.query(self.fake_A)
+        self.loss_D_B = self.backward_D_basic(self.netD_B, self.real_A, fake_A)
+
+    def backward_G(self):
+        """Calculate the loss for generators G_A and G_B"""
+        lambda_idt = self.opt.lambda_identity
+        lambda_A = self.opt.lambda_A
+        lambda_B = self.opt.lambda_B
+        # Identity loss
+        if lambda_idt > 0:
+            # G_A should be identity if real_B is fed: ||G_A(B) - B||
+            self.idt_A = self.netG_A(self.real_mask_B)
+            self.loss_idt_A = self.criterionIdt(self.idt_A, self.real_B) * lambda_B * lambda_idt
+            # G_B should be identity if real_A is fed: ||G_B(A) - A||
+            self.idt_B = self.netG_B(self.real_mask_A)
+            self.loss_idt_B = self.criterionIdt(self.idt_B, self.real_A) * lambda_A * lambda_idt
+        else:
+            self.loss_idt_A = 0
+            self.loss_idt_B = 0
+            
+        # MSSIM LOSS 
+        self.loss_mssim_A = (1 - self.msssim(self.rec_A, self.real_A)) * lambda_A
+        self.loss_mssim_B = (1 - self.msssim(self.rec_B, self.real_B)) * lambda_B
+            
+        # Feature Loss 
+        features_fake_A, gram_matrix_fake_A = self.feature_extractor(self.fake_A)
+        features_real_A, gram_matrix_real_A = self.feature_extractor(self.real_A)
+        features_fake_B, gram_matrix_fake_B = self.feature_extractor(self.fake_B)
+        features_real_B, gram_matrix_real_B = self.feature_extractor(self.real_B)
+        
+        self.loss_features_A = torch.nn.L1Loss()(features_fake_A, features_real_A)
+        self.loss_features_B = torch.nn.L1Loss()(features_fake_B, features_real_B)
+        self.loss_gram_A = torch.nn.L1Loss()(gram_matrix_fake_A, gram_matrix_real_A) / 100
+        self.loss_gram_B = torch.nn.L1Loss()(gram_matrix_fake_B, gram_matrix_real_B) / 100 
+
+        # GAN loss D_A(G_A(A))
+        self.loss_G_A = self.criterionGAN(self.netD_A(self.fake_B), True)
+        # GAN loss D_B(G_B(B))
+        self.loss_G_B = self.criterionGAN(self.netD_B(self.fake_A), True)
+        # Forward cycle loss || G_B(G_A(A)) - A||
+        self.loss_cycle_A = self.criterionCycle(self.rec_A, self.real_A) * lambda_A
+        # Backward cycle loss || G_A(G_B(B)) - B||
+        self.loss_cycle_B = self.criterionCycle(self.rec_B, self.real_B) * lambda_B
+        # combined loss and calculate gradients
+        self.loss_G = self.loss_G_A + self.loss_G_B + self.loss_cycle_A + self.loss_cycle_B + self.loss_idt_A + self.loss_idt_B + self.loss_features_A + self.loss_features_B + self.loss_mssim_A + self.loss_mssim_B + self.loss_gram_A + self.loss_gram_B
+        self.loss_G.backward()
+
+    def optimize_parameters(self):
+        """Calculate losses, gradients, and update network weights; called in every training iteration"""
+        torch.autograd.set_detect_anomaly(True)
+        self.set_requires_grad([self.netD_A, self.netD_B], False)  # Ds require no gradients when optimizing Gs
+        for _ in range(2):
+            # forward
+            self.forward()      # compute fake images and reconstruction images.
+            # G_A and G_B
+            self.optimizer_G.zero_grad()  # set G_A and G_B's gradients to zero
+            self.feature_extractor.eval()
+            self.backward_G()             # calculate gradients for G_A and G_B
+            self.optimizer_G.step()       # update G_A and G_B's weights
+        # D_A and D_B
+        self.set_requires_grad([self.netD_A, self.netD_B], True)
+        self.optimizer_D.zero_grad()   # set D_A and D_B's gradients to zero
+        self.backward_D_A()      # calculate gradients for D_A
+        self.backward_D_B()      # calculate graidents for D_B
+        self.optimizer_D.step()  # update D_A and D_B's weights
